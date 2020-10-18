2e558ec862ecfc3d1bdd414230712453005c97ee
Katharina Wolf <wolfkatharina89@gmail.com>
2020-10-18 11:36:29 +0200

    Registration set

---
+++ db.js
@@ -0,0 +1,50 @@
+const spicedPg = require("spiced-pg");
+const db = spicedPg(
+    process.env.DATABASE_URL ||
+        "postgres:postgres:postgres@localhost:5432/food-app"
+);
+
+// ------------------- users table ------------------- //
+
+module.exports.addUser = (firstname, lastname, email, password) => {
+    const q = `INSERT INTO users (firstname, lastname, email, password) VALUES ($1, $2, $3, $4) 
+    RETURNING id`;
+
+    const params = [firstname, lastname, email, password];
+    return db.query(q, params);
+};
+
+module.exports.loginUser = (email) => {
+    const q = `SELECT password, id FROM users WHERE email = $1`;
+    const params = [email];
+    return db.query(q, params);
+};
+
+module.exports.getUserInfo = (id) => {
+    const q = `SELECT * FROM users WHERE id = $1`;
+    const params = [id];
+    return db.query(q, params);
+};
+
+module.exports.addNewPassword = (email, password) => {
+    const q = `UPDATE users SET password=$2 WHERE email=$1 RETURNING *`;
+    const params = [email, password];
+    return db.query(q, params);
+};
+// ------------------- password_reset_codes table ------------------- //
+
+module.exports.addResetCode = (email, code) => {
+    const q = `INSERT INTO password_reset_codes (email, code) 
+    VALUES ($1, $2) RETURNING id`;
+    const params = [email, code];
+    return db.query(q, params);
+};
+
+module.exports.getCode = (email) => {
+    const q = `SELECT code FROM password_reset_codes WHERE email=$1 AND 
+    CURRENT_TIMESTAMP - created_at < INTERVAL '10 minutes' 
+    ORDER BY created_at DESC
+    LIMIT 1`;
+    const params = [email];
+    return db.query(q, params);
+};
--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const express = require("express");
 const app = express();
 const compression = require("compression");
-// const config is for image upload
-// const config = require("./config.json");
 
+const cookieSession = require("cookie-session");
+// const config is for image upload
+const config = require("./config.json");
+const { compare, hash } = require("./bcrypt.js");
+const csurf = require("csurf");
+const db = require("./db.js");
+const ses = require("./ses.js");
+const s3 = require("./s3.js");
+const multer = require("multer");
+const uidSafe = require("uid-safe");
+const path = require("path");
+
+const diskStorage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, __dirname + "/uploads");
+    },
+    filename: function (req, file, callback) {
+        uidSafe(24).then(function (uid) {
+            callback(null, uid + path.extname(file.originalname));
+        });
+    },
+});
+const uploader = multer({
+    storage: diskStorage,
+    limits: {
+        fileSize: 2097152,
+    },
+});
 app.use(compression());
 app.use(express.static("./public"));
 app.use(express.json());
@@ -13,6 +39,27 @@ app.use(
         extended: false,
     })
 );
+
+// cookie session
+let secrets;
+process.env.NODE_ENV === "production"
+    ? (secrets = process.env)
+    : (secrets = require("./secrets"));
+app.use(
+    cookieSession({
+        secret: `${secrets.sessionSecret}`,
+        maxAge: 1000 * 60 * 60 * 24,
+    })
+);
+
+// cookie Session token protection
+app.use(csurf());
+
+app.use(function (req, res, next) {
+    console.log("token");
+    res.cookie("mytoken", req.csrfToken());
+    next();
+});
 
 app.use(function (req, res, next) {
     res.set("x-frame-options", "DENY");
@@ -30,8 +77,12 @@ if (process.env.NODE_ENV != "production") {
 } else {
     app.use("/bundle.js", (req, res) => res.sendFile(`${__dirname}/bundle.js`));
 }
+////////////////////////////////////////////////////////////////////////////////
+/* ------------------------------   ALL ROUTES  ---------------------------- */
+///////////////////////////////////////////////////////////////////////////////
+
 ////////////////////////////////////////////////
-/* --------------   ALL ROUTES  ------------- */
+/* ----------------  WELCOME  --------------- */
 ////////////////////////////////////////////////
 app.get("/welcome", function (req, res) {
     console.log("in get /welcome: ", req.session);
@@ -44,8 +95,77 @@ app.get("/welcome", function (req, res) {
     // }
 });
 
+////////////////////////////////////////////////
+/* ---------------  REGISTER  --------------- */
+////////////////////////////////////////////////
+
+app.post("/register", (req, res) => {
+    console.log("user info, req.body: ", req.body);
+    const { firstname, lastname, email } = req.body;
+    const plainPassword = req.body.password;
+
+    if (!firstname || !lastname || !email || !plainPassword) {
+        console.log("Missing input!");
+        res.json({ success: false });
+    } else {
+        hash(plainPassword).then((password) => {
+            console.log("plainPassword got hashed -> : ", password);
+            db.addUser(firstname, lastname, email, password)
+                .then((result) => {
+                    console.log("userId from DB: ", result.rows[0].id);
+                    req.session.userId = result.rows[0].id;
+                    res.json({ success: true });
+                })
+                .catch((err) => {
+                    console.log("Error in POST / register addUser", err);
+                    res.json({ success: false });
+                });
+        });
+    }
+});
+
+////////////////////////////////////////////////
+/* -----------------  LOGIN ----------------- */
+////////////////////////////////////////////////
+
+app.post("/login", (req, res) => {
+    console.log("user info login, req.body: ", req.body);
+    const email = req.body.email;
+    const plainPassword = req.body.password;
+
+    db.loginUser(email)
+        .then((hashedPassword) => {
+            const password = hashedPassword.rows[0].password;
+            // console.log("password ", password);
+            // console.log(
+            //     "hashedPassword.rows[0].id ",
+            //     hashedPassword.rows[0].id
+            // );
+            compare(plainPassword, password)
+                .then((userExists) => {
+                    console.log("userExists: ", userExists);
+                    if (userExists) {
+                        // console.log("user Exists GO ON!");
+                        req.session.userId = hashedPassword.rows[0].id;
+                        res.json({ success: true });
+                    } else {
+                        console.log("Wrong email pw combination!");
+                        res.json({ success: false });
+                    }
+                })
+                .catch((err) => {
+                    console.log("error in catch block compare", err);
+                    res.json({ success: false });
+                });
+        })
+        .catch((err) => {
+            console.log("err in catch block loginUser", err);
+            res.json({ success: false });
+        });
+});
+
 app.get("/user/logged-in", function (req, res) {
-    console.log("HELLO USER PAGE! userId: ");
+    console.log("HELLO USER PAGE! userId: ", req.session.userId);
     // const userId = req.session.userId;
     // db.getUserInfo(userId)
     //     .then((result) => {
